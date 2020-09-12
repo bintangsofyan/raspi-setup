00f113408c31b894d7f7269ec1b359a30983a14f
bintangsofyan <bintang.sofyan@live.com>
2020-09-13 02:22:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo -e "${BGreen}Start Shell Script${reset}"
 
 echo ""
 echo -e "${BGreen}setup static ip to 192.168.1.254 with default gateway 192.168.1.1${reset}"
-printf '%s\n' 'interface eth0' '' 'static ip_address=192.168.0.254/24' '' 'static routers=192.168.1.1' >> /etc/systemd/system/node_exporter.service
+printf '%s\n' 'interface eth0' '' 'static ip_address=192.168.0.254/24' '' 'static routers=192.168.1.1' >> /etc/dhcpcd.conf
 
 echo ""
 echo -e "${BGreen}get into /root/${reset}"
